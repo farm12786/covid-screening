693b76d0c33377658fb7d8b1d877b32741da5410
DESKTOP-4EGO68O\Natawat <natawat14277@hotmail.com>
2021-10-12 15:44:52 +0700

    desktop responsive

---
+++ dist/precache-manifest.77ad6aabe30fd5244e9cb55a3314c7dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "363ca2cdbacaafa11986",
+    "url": "/css/app.bbe44fd2.css"
+  },
+  {
+    "revision": "d6de5988a42b89875a73",
+    "url": "/css/chunk-vendors.5541730d.css"
+  },
+  {
+    "revision": "32ada6ca2fc7112dfbf68bdb4c3a5d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363ca2cdbacaafa11986",
+    "url": "/js/app.222e75ff.js"
+  },
+  {
+    "revision": "d6de5988a42b89875a73",
+    "url": "/js/chunk-vendors.0c568766.js"
+  },
+  {
+    "revision": "8d7a60377facc8fac6352db3e4010514",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
